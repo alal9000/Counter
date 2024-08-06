e92ef95959c2b6afe1577cabc890454441f34452
Aaron Lal <lucidlear@gmail.com>
2024-08-07 08:52:32 +1000

    added Clock and test classes for Clock and Counter

---
+++ Counter/Clock.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public class Clock
+    {
+        private Counter _seconds;
+        private Counter _minutes;
+        private Counter _hours;
+
+        public Clock(string name)
+        {
+            _seconds = new Counter(name + " seconds");
+            _minutes = new Counter(name + " minutes");
+            _hours = new Counter(name + " hours");
+        }
+
+        public void Tick()
+        {
+            _seconds.Increment();
+
+            if (_seconds.Ticks == 60)
+            {
+                _minutes.Increment();
+            }
+
+            if (_minutes.Ticks == 60)
+            {
+                _hours.Increment();
+            }
+
+            if (_hours.Ticks == 25)
+            {
+                _hours.Reset();
+            }
+        }
+
+        public void Reset()
+        {
+            _seconds.Reset();
+            _minutes.Reset();
+            _hours.Reset();
+        }
+
+        public void GetCurrentTime()
+        {
+            Console.WriteLine("{0}:{1}:{2}", 
+                _hours.Ticks, 
+                _minutes.Ticks, 
+                _seconds.Ticks
+            );          
+        }
+
+
+    }
+}
---
+++ Counter/TestCounter.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    [TestFixture]
+    public class TestCounter
+    {
+        // arrange
+        private Counter _counter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+            _counter = new Counter("test");
+        }
+
+        // act and assert
+        [Test]
+        public void TestInitialise()
+        {
+            Assert.That(_counter.Ticks, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestIncrement()
+        {
+            _counter.Increment();
+            Assert.That(_counter.Ticks, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestIncrementMultiple()
+        {
+            _counter.Increment();
+            _counter.Increment();
+            _counter.Increment();
+            Assert.That(_counter.Ticks, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void TestReset()
+        {
+            _counter.Increment();
+            _counter.Increment();
+            _counter.Increment();
+            _counter.Reset();
+            Assert.That(_counter.Ticks, Is.EqualTo(0));
+        }
+    }
+}
